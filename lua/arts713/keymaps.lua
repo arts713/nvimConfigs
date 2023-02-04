aff0c367401a00f72283641ad7eabe598a1c26d9
arts713 <development@arts.mozmail.com>
2023-02-04 17:05:43 +0100

    base. nvimtree. add hotkeys for sidebar

--- lua/arts713/keymaps.lua
+++ lua/arts713/keymaps.lua
@@ -7,7 +7,9 @@ keymap("i", "jj", "<ESC>", opts)
 
 -- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 -- vim.keymap.set("n", "<leader>e", ":Lexplore")
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<cr>", opts)
+vim.keymap.set("n", "<leader>fe", ":NvimTreeFindFile<cr>", opts)
+vim.keymap.set("n", "<leader>b", ":NvimTreeToggle<cr>", opts)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
